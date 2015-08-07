1218f5819046492cc2ea605867ddee9bfa95c93d
Luther Lowry <STLCajun@Luthers-MacBook-Pro.local>
2015-08-07 14:48:32 -0500

    Additional cleanup items

--- src/Storage/Mongo/FluentAccessToken.php
+++ src/Storage/Mongo/FluentAccessToken.php
@@ -1,149 +1,153 @@
 <?php
 
-/*
- * This file is part of OAuth 2.0 Laravel.
- *
- * (c) Luca Degasperi <packages@lucadegasperi.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace LucaDegasperi\OAuth2Server\Storage\Mongo;
-
-use Carbon\Carbon;
-use League\OAuth2\Server\Entity\AccessTokenEntity;
-use League\OAuth2\Server\Entity\ScopeEntity;
-use League\OAuth2\Server\Storage\AccessTokenInterface;
-
-/**
- * This is the fluent access token class.
- *
- * @author Luca Degasperi <packages@lucadegasperi.com>
- */
-class FluentAccessToken extends AbstractFluentAdapter implements AccessTokenInterface
-{
-    /**
-     * Get an instance of Entities\AccessToken.
-     *
-     * @param string $token The access token
-     *
-     * @return null|AbstractTokenEntity
-     */
-    public function get($token)
-    {
-        $result = $this->getConnection()->table('oauth_access_tokens')
-                ->where('oauth_access_tokens.id', $token)
-                ->first();
-
-        if (is_null($result)) {
-            return;
-        }
-
-        return (new AccessTokenEntity($this->getServer()))
-               ->setId($result->id)
-               ->setExpireTime((int) $result->expire_time);
-    }
-
     /*
-    public function getByRefreshToken(RefreshTokenEntity $refreshToken)
-    {
-        $result = $this->getConnection()->table('oauth_access_tokens')
-                ->select('oauth_access_tokens.*')
-                ->join('oauth_refresh_tokens', 'oauth_access_tokens.id', '=', 'oauth_refresh_tokens.access_token_id')
-                ->where('oauth_refresh_tokens.id', $refreshToken->getId())
-                ->first();
+	 * This file is part of OAuth 2.0 Laravel.
+	 *
+	 * (c) Luca Degasperi <packages@lucadegasperi.com>
+	 *
+	 * For the full copyright and license information, please view the LICENSE
+	 * file that was distributed with this source code.
+	 */
 
-        if (is_null($result)) {
-            return null;
-        }
+    namespace LucaDegasperi\OAuth2Server\Storage\Mongo;
 
-        return (new AccessTokenEntity($this->getServer()))
-               ->setId($result->id)
-               ->setExpireTime((int)$result->expire_time);
-    }
-    */
+    use Carbon\Carbon;
+    use League\OAuth2\Server\Entity\AccessTokenEntity;
+    use League\OAuth2\Server\Entity\ScopeEntity;
+    use League\OAuth2\Server\Storage\AccessTokenInterface;
 
     /**
-     * Get the scopes for an access token.
+     * This is the fluent access token class.
      *
-     * @param \League\OAuth2\Server\Entity\AccessTokenEntity $token The access token
-     *
-     * @return array Array of \League\OAuth2\Server\Entity\ScopeEntity
+     * @author Luca Degasperi <packages@lucadegasperi.com>
      */
-    public function getScopes(AccessTokenEntity $token)
+    class FluentAccessToken extends AbstractFluentAdapter implements AccessTokenInterface
     {
-        $result = $this->getConnection()->table('oauth_access_token_scopes')
-                ->select('oauth_scopes.*')
-                ->join('oauth_scopes', 'oauth_access_token_scopes.scope_id', '=', 'oauth_scopes.id')
-                ->where('oauth_access_token_scopes.access_token_id', $token->getId())
+        /**
+         * Get an instance of Entities\AccessToken.
+         *
+         * @param string $token The access token
+         *
+         * @return null|AbstractTokenEntity
+         */
+        public function get($token)
+        {
+            $result = $this->getConnection()->table('oauth_access_tokens')
+                ->where('id', $token)
+                ->first();
+
+            if (is_null($result)) {
+                return;
+            }
+
+            return (new AccessTokenEntity($this->getServer()))
+                ->setId($result['id'])
+                ->setExpireTime((int) $result['expire_time']);
+        }
+
+        /*
+		public function getByRefreshToken(RefreshTokenEntity $refreshToken)
+		{
+			$result = $this->getConnection()->table('oauth_access_tokens')
+					->select('oauth_access_tokens.*')
+					->join('oauth_refresh_tokens', 'oauth_access_tokens.id', '=', 'oauth_refresh_tokens.access_token_id')
+					->where('oauth_refresh_tokens.id', $refreshToken->getId())
+					->first();
+
+			if (is_null($result)) {
+				return null;
+			}
+
+			return (new AccessTokenEntity($this->getServer()))
+				   ->setId($result->id)
+				   ->setExpireTime((int)$result->expire_time);
+		}
+		*/
+
+        /**
+         * Get the scopes for an access token.
+         *
+         * @param \League\OAuth2\Server\Entity\AccessTokenEntity $token The access token
+         *
+         * @return array Array of \League\OAuth2\Server\Entity\ScopeEntity
+         */
+        public function getScopes(AccessTokenEntity $token)
+        {
+            $result = $this->getConnection()->table('oauth_access_token_scopes')
+                ->where('access_token_id', $token->getId())
                 ->get();
 
-        $scopes = [];
+            $scopes = [];
 
-        foreach ($result as $scope) {
-            $scopes[] = (new ScopeEntity($this->getServer()))->hydrate([
-               'id' => $scope->id,
-                'description' => $scope->description,
+            foreach ($result as $scope) {
+
+                $scope = $this->getConnection()->table('oauth_scopes')
+                    ->where('id', $scope['scope_id'])
+                    ->get();
+
+                $scopes[] = (new ScopeEntity($this->getServer()))->hydrate([
+                    'id' => $scope['id'],
+                    'description' => $scope['description'],
+                ]);
+
+            }
+
+            return $scopes;
+        }
+
+        /**
+         * Creates a new access token.
+         *
+         * @param string $token The access token
+         * @param int $expireTime The expire time expressed as a unix timestamp
+         * @param string|int $sessionId The session ID
+         *
+         * @return \League\OAuth2\Server\Entity\AccessTokenEntity
+         */
+        public function create($token, $expireTime, $sessionId)
+        {
+            $this->getConnection()->table('oauth_access_tokens')->insert([
+                'id' => $token,
+                'expire_time' => $expireTime,
+                'session_id' => $sessionId,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+
+            return (new AccessTokenEntity($this->getServer()))
+                ->setId($token)
+                ->setExpireTime((int) $expireTime);
+        }
+
+        /**
+         * Associate a scope with an access token.
+         *
+         * @param \League\OAuth2\Server\Entity\AccessTokenEntity $token The access token
+         * @param \League\OAuth2\Server\Entity\ScopeEntity $scope The scope
+         *
+         * @return void
+         */
+        public function associateScope(AccessTokenEntity $token, ScopeEntity $scope)
+        {
+            $this->getConnection()->table('oauth_access_token_scopes')->insert([
+                'access_token_id' => $token->getId(),
+                'scope_id' => $scope->getId(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ]);
         }
 
-        return $scopes;
+        /**
+         * Delete an access token.
+         *
+         * @param \League\OAuth2\Server\Entity\AccessTokenEntity $token The access token to delete
+         *
+         * @return void
+         */
+        public function delete(AccessTokenEntity $token)
+        {
+            $this->getConnection()->table('oauth_access_tokens')
+                ->where('id', $token->getId())
+                ->delete();
+        }
     }
-
-    /**
-     * Creates a new access token.
-     *
-     * @param string $token The access token
-     * @param int $expireTime The expire time expressed as a unix timestamp
-     * @param string|int $sessionId The session ID
-     *
-     * @return \League\OAuth2\Server\Entity\AccessTokenEntity
-     */
-    public function create($token, $expireTime, $sessionId)
-    {
-        $this->getConnection()->table('oauth_access_tokens')->insert([
-            'id' => $token,
-            'expire_time' => $expireTime,
-            'session_id' => $sessionId,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-
-        return (new AccessTokenEntity($this->getServer()))
-               ->setId($token)
-               ->setExpireTime((int) $expireTime);
-    }
-
-    /**
-     * Associate a scope with an access token.
-     *
-     * @param \League\OAuth2\Server\Entity\AccessTokenEntity $token The access token
-     * @param \League\OAuth2\Server\Entity\ScopeEntity $scope The scope
-     *
-     * @return void
-     */
-    public function associateScope(AccessTokenEntity $token, ScopeEntity $scope)
-    {
-        $this->getConnection()->table('oauth_access_token_scopes')->insert([
-            'access_token_id' => $token->getId(),
-            'scope_id' => $scope->getId(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-    }
-
-    /**
-     * Delete an access token.
-     *
-     * @param \League\OAuth2\Server\Entity\AccessTokenEntity $token The access token to delete
-     *
-     * @return void
-     */
-    public function delete(AccessTokenEntity $token)
-    {
-        $this->getConnection()->table('oauth_access_tokens')
-        ->where('oauth_access_tokens.id', $token->getId())
-        ->delete();
-    }
-}
